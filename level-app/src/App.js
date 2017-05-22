aadb1327a720e0b3b684c3a2699507ddfc2ee06f
sopheak <sopheakneak@live.com>
2017-05-22 12:07:30 -0700

    configured the landing page structure

--- level-app/src/App.js
+++ level-app/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     this.state = {
       selectedClass: "",
       selectedLevel: "",
-      selectedCombo:{},
+      selectedCombo: {},
       classButtonTitle: "Character Class",
       levelsArray: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
       levelButtonTitle: "",
@@ -30,17 +30,25 @@ class App extends Component {
   }
   handleSubmit = (event) => {
     event.preventDefault();
-    var nextLevel = this.state.recipeStep++;
+    var nextLevel = this.state.recipeStep + 1;
     var foundCombo;
-    _.forEach(data.Classes, (value)=>{
-      if(value.Class === this.state.selectedClass) {
+    _.forEach(data.Classes, (value) => {
+      if (value.Class === this.state.selectedClass) {
         foundCombo = value;
       }
     })
-    var newState = update(this.selectedCombo,{$set:foundCombo});
-    //console.log(newState);
-    this.setState({ openLanding: false, recipeStep: 1, selectedCombo: newState})
-    console.log(this.state);
+    this.setState({ openLanding: false, recipeStep: nextLevel, selectedCombo: foundCombo });
+  }
+
+  handleNext = (event) => {
+    event.preventDefault();
+    var nextLevel = this.state.recipeStep + 1;
+    this.setState({ recipeStep: nextLevel });
+  }
+  handleBack = (event) => {
+    event.preventDefault();
+    var nextLevel = this.state.recipeStep - 1;
+    this.setState({ recipeStep: nextLevel });
   }
   render() {
     var availableClasses = _.map(data.Classes, (value, index) => {
@@ -56,7 +64,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        {this.state.openLanding &&
+        {this.state.recipeStep === 0 &&
           <div>
             <div>
               <h2>Select a Class</h2>
@@ -74,16 +82,22 @@ class App extends Component {
           </div>
         }
         {this.state.recipeStep === 1 &&
-          <div><p>First Page</p></div>
+          <div><p>First Page</p>
+            <Button bsStyle='primary' href='.'>Back</Button>
+            <Button bsStyle='primary' onClick={this.handleNext.bind(this)}>Next</Button>
+          </div>
         }
-
-
+        {this.state.recipeStep === 2 &&
+          <div>
+            <p>Second Page</p>
+            <Button bsStyle='primary' onClick={this.handleBack.bind(this)}>Back</Button>
+          </div>
+        }
       </div>
-
     );
   }
 }
-/*
+
 class FeatureList extends Component {
   constructor(props) {
     super(props);
@@ -94,37 +108,13 @@ class FeatureList extends Component {
   // getInitialState() {
   //   return { showModal: false };
   // }
-  open() {
-    this.setState({ showModal: true });
-  }
-  close() {
-    this.setState({ showModal: false });
-  }
-
   render() {
     return (
       <div>
-        <p>Click to get the full Modal experience!</p>
-
-        <Button bsStyle="primary" bsSize="large" onClick={this.open}>
-          Launch demo modal
-        </Button>
-
-        <Modal show={this.state.showModal} onHide={this.close}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.close}>Close</Button>
-          </Modal.Footer>
-        </Modal>
       </div>
     );
   }
-
-}*/
+}
 
 
 export default App;
