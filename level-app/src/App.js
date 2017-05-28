7765c4b34604d15212ff4c4a4f0fda0f40d5a4fd
sopheak <sopheakneak@live.com>
2017-05-27 20:30:50 -0700

    configured react router layout

--- level-app/src/App.js
+++ level-app/src/App.js
@@ -8,6 +8,10 @@ import HeaderTitle from './Header.js';
 import Feature from './Feature.js';
 import request from 'request';
 import controller from './Controller.js';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import MainSelection from './MainSelection.js';
+import { Layout, Header, Drawer, Navigation, Content } from 'react-mdl';
+
 //import update from 'immutability-helper';
 //import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 //import $ from 'jquery';
@@ -16,52 +20,66 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedClass: "",
-      selectedLevel: "",
-      selectedCombo: {},
-      classButtonTitle: "Character Class",
-      levelsArray: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-      levelButtonTitle: "",
-      openLanding: true,
-      recipeStep: 0,
-      test: {}
-    };
+      selectedObject: {}
+    }
+    // this.state = {
+    //   selectedClass: "",
+    //   selectedLevel: "",
+    //   selectedCombo: {},
+    //   classButtonTitle: "Character Class",
+    //   levelsArray: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    //   levelButtonTitle: "",
+    //   openLanding: true,
+    //   recipeStep: 0,
+    //   test: {}
+    // };
   }
-  handleClass = (eventKey) => {
-    this.setState({ selectedClass: eventKey, classButtonTitle: eventKey });
-  }
-  handleLevel = (eventKey) => {
-    this.setState({ selectedLevel: eventKey, levelButtonTitle: eventKey });
-  }
-  handleSubmit = (event) => {
-    event.preventDefault();
-    var nextLevel = this.state.recipeStep + 1;
-    var foundCombo;
-    _.forEach(data.Classes, (value) => {
-      if (value.Class === this.state.selectedClass) {
-        foundCombo = value;
-      }
-    })
-    this.setState({ openLanding: false, recipeStep: nextLevel, selectedCombo: foundCombo });
-  }
+  // handleClass = (eventKey) => {
+  //   this.setState({ selectedClass: eventKey, classButtonTitle: eventKey });
+  // }
+  // handleLevel = (eventKey) => {
+  //   this.setState({ selectedLevel: eventKey, levelButtonTitle: eventKey });
+  // }
+  // handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   var nextLevel = this.state.recipeStep + 1;
+  //   var foundCombo;
+  //   _.forEach(data.Classes, (value) => {
+  //     if (value.Class === this.state.selectedClass) {
+  //       foundCombo = value;
+  //     }
+  //   })
+  //   this.setState({ openLanding: false, recipeStep: nextLevel, selectedCombo: foundCombo });
+  // }
 
-  handleNext = (event) => {
-    event.preventDefault();
-    var nextLevel = this.state.recipeStep + 1;
-    this.setState({ recipeStep: nextLevel });
+  handleFound = (returnedObject) => {
+    this.setState({ selectedObject: returnedObject });
   }
-  handleBack = (event) => {
-    event.preventDefault();
-    var nextLevel = this.state.recipeStep - 1;
-    this.setState({ recipeStep: nextLevel });
-  }
-  handleTest = (event) => {
-    let thisComponent = this;
-    controller.search('http://www.dnd5eapi.co/api/features/1')
-      .then(function(data){
-        thisComponent.setState({test:data})
-      })
-  }
+  //  handleBack = () => {
+  //   this.setState({selectedObject: {}});
+  // }
+
+  // componentWillMount() {
+  //   this.setState({selectedObject: {}});
+  // } 
+
+  // handleNext = (event) => {
+  //   event.preventDefault();
+  //   var nextLevel = this.state.recipeStep + 1;
+  //   this.setState({ recipeStep: nextLevel });
+  // }
+  // handleBack = (event) => {
+  //   event.preventDefault();
+  //   var nextLevel = this.state.recipeStep - 1;
+  //   this.setState({ recipeStep: nextLevel });
+  // }
+  // handleTest = (event) => {
+  //   let thisComponent = this;
+  //   controller.search('http://www.dnd5eapi.co/api/features/1')
+  //     .then(function (data) {
+  //       thisComponent.setState({ test: data })
+  //     })
+  // }
 
   // getData = (url) => {
   //   return fetch(url)
@@ -76,15 +94,72 @@ class App extends Component {
   // }
 
   render() {
-    var availableClasses = _.map(data.Classes, (value, index) => {
-      var keyname = value.Class;
-      return <MenuItem eventKey={keyname} key={index}>{keyname}</MenuItem>
-    });
-    var availableLevels = _.map(this.state.levelsArray, (value) => {
-      return <MenuItem eventKey={value} key={value}>Level {value}</MenuItem>
-    });
+    // var availableClasses = _.map(data.Classes, (value, index) => {
+    //   var keyname = value.Class;
+    //   return <MenuItem eventKey={keyname} key={index}>{keyname}</MenuItem>
+    // });
+    // var availableLevels = _.map(this.state.levelsArray, (value) => {
+    //   return <MenuItem eventKey={value} key={value}>Level {value}</MenuItem>
+    // });
     return (
-      <div className="container">
+
+      <Router>
+        <div className="demo-big-content">
+          <Layout fixedHeader fixedDrawer>
+            <Header title="Title" >
+            </Header>
+            <Drawer title="Title">
+              <Navigation>
+                {Object.keys(this.state.selectedObject).length > 0 &&
+                  <Link to='/Features'>Add Features</Link>
+                }
+                {Object.keys(this.state.selectedObject).length > 0 &&
+                  <Link to='/SpellSlots'>Incease Spell Slots</Link>
+                }
+                {Object.keys(this.state.selectedObject).length > 0 &&
+                  <Link to='/Spells'>Add Spells</Link>
+                }
+                {Object.keys(this.state.selectedObject).length > 0 &&
+                  <Link to='/Cantrips'>Add Cantrips</Link>
+                }
+                <Link to='/'>Different Level or Class?</Link>
+              </Navigation>
+            </Drawer>
+            <Content>
+              <div>
+                <Route exact path='/' component={(props) => <MainSelection handleFound={this.handleFound} />} />
+                <Route path='/Features' component={(props) => <Feature classObject={this.state.selectedObject} />} />
+              </div>
+            </Content>
+          </Layout>
+
+
+          {/*<div className="container">
+          <h2>Select a Class</h2>
+          <SplitButton bsStyle="success" title={this.state.classButtonTitle} id='split-button-basic' onSelect={this.handleClass.bind(this)}>
+            {availableClasses}
+          </SplitButton>
+          <h2>Select a Level</h2>
+          <SplitButton bsStyle="success" title={"Level " + this.state.levelButtonTitle} id='split-button-basic' onSelect={this.handleLevel.bind(this)}>
+            {availableLevels}
+          </SplitButton>
+        </div>*/}
+          {/*<div style={{ height: '300px', position: 'relative' }}>
+          <Layout fixedHeader>
+            <Header title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>The Title</strong></span>}>
+            </Header>
+            <Drawer title="Title">
+              <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+              </Navigation>
+            </Drawer>
+            <Content />
+          </Layout>
+        </div>*/}
+          {/*<div className="container">
         {this.state.recipeStep === 0 &&
           <div>
             <div>
@@ -118,25 +193,9 @@ class App extends Component {
             <Button bsStyle='primary' onClick={this.handleBack.bind(this)}>Back</Button>
           </div>
         }
-      </div>
-    );
-  }
-}
-
-class FeatureList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showModal: false
-    };
-  }
-  // getInitialState() {
-  //   return { showModal: false };
-  // }
-  render() {
-    return (
-      <div>
-      </div>
+      </div>*/}
+        </div>
+      </Router>
     );
   }
 }
--- level-app/src/Feature.js
+++ level-app/src/Feature.js
@@ -2,46 +2,89 @@ import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import HeaderTitle from './Header.js';
 import controller from './Controller';
+import { Redirect } from 'react-router-dom';
 
 class Feature extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            pages: this.props.classObject.length - 1,
+            pages: 0,
+            options: [],
             current: 1,
-            test: {},
-            links:['http://www.dnd5eapi.co/api/features/1','http://www.dnd5eapi.co/api/features/2']
+            received: {},
+            links: ['http://www.dnd5eapi.co/api/features/1', 'http://www.dnd5eapi.co/api/features/2'],
+            redirect: false,
+            path: ''
         };
     }
     handleNextFeature = (event) => {
         var next = this.state.current + 1;
-        this.setState({ current: next, test: {} });
+        this.setState({ current: next, received: {} });
         controller.search(this.state.links[next - 1])
             .then(data => {
-                this.setState({ test: data })
+                this.setState({ received: data })
             })
     };
     handleBackFeature = (event) => {
         var next = this.state.current - 1;
         //this.setState({ current: next });
-        this.setState({ current: next, test: {} });
-        controller.search(this.state.links[next-1])
+        this.setState({ current: next, received: {} });
+        controller.search(this.state.links[next - 1])
             .then(data => {
-                this.setState({ test: data })
+                this.setState({ received: data })
             })
     };
+    handler = (event) => {
+        let path = event.target.value;
+        this.setState({ redirect: true, path: path });
+        if (path === '/') {
+            window.location.reload();
+        }
+
+    }
+    // componentWillMount() {
+    //     //let thisComponent = this;
+    //     controller.search(this.state.links[0])
+    //         .then(data => {
+    //             this.setState({ test: data })
+    //         })
+    // };
+
     componentWillMount() {
-        //let thisComponent = this;
-        controller.search(this.state.links[0])
-            .then(data => {
-                this.setState({ test: data })
-            })
+        if (Object.keys(this.props.classObject).length !== 0) {
+            let combo = this.props.classObject.classObject.LevelRecipes[this.props.classObject.classLevel - 1];
+            this.setState({ pages: combo.length - 1, options: combo });
+            controller.search(this.state.links[0])
+                .then(data => {
+                    //return data;
+                    this.setState({ received: data })
+                })
+            //this.setState({ pages: combo.length - 1, options: combo, test: foundData });
+        }
     };
 
-    componentDidMount() {
-        console.log("this is " + 2);
+    // componentWillReceiveProps(newProps) {
+    //     if (newProps !== this.props) {
+    //         if (Object.keys(newProps.classObject).length !== 0) {
+    //             let combo = newProps.classObject.classObject.LevelRecipes[newProps.classObject.classLevel - 1];
+    //             this.setState({ pages: combo.length - 1, options: combo });
+    //             controller.search(this.state.links[0])
+    //                 .then(data => {
+    //                     //return data;
+    //                     this.setState({ test: data })
+    //                 })
+    //             //this.setState({ pages: combo.length - 1, options: combo, test: foundData });
+    //         }
+    //     }
+    // };
 
-    };
+    // componentDidMount() {
+    //     console.log(this.props.classObject);
+    //     let combo = this.props.classObject.classObject;
+    //     console.log("post combo",combo.LevelRecipes);
+    //     this.setState({options:combo});
+
+    // };
 
     // componentWillReceiveProps(nextProps){
     //     console.log("recieve: ",nextProps);
@@ -53,24 +96,28 @@ class Feature extends Component {
     // };
 
     render() {
-        // var found = this.state.test.desc;
-        // //console.log(found)
-        if (this.state.test.desc !== undefined) {
-            var para = this.state.test.desc.map(function (data, index) {
+        //var found = this.state.test.desc;
+        //console.log(found)
+        if (this.state.received.desc !== undefined) {
+            var para = this.state.received.desc.map(function (data, index) {
                 return <p key={index}>{data}</p>
             });
         }
+        if (this.state.redirect) {
+            return <Redirect push to={this.state.path} />;
+        }
+
         return (
             <div>
                 <div>
-                    <HeaderTitle classTitle={this.props.classTitle} levelTitle={this.props.levelTitle}
-                        featureName={this.props.classObject[this.state.current]} />
+                    <HeaderTitle classTitle={this.props.classObject.className} levelTitle={this.props.classObject.classLevel}
+                        featureName={this.state.options[this.state.current]} />
                     {para}
                     {/* use the string to find description <p>{this.props.classObject[this.state.current]}</p>*/}
                 </div>
-                {this.state.current == 1 &&
+                {this.state.current === 1 &&
                     <div>
-                        <Button onClick={this.props.backHandler}>Back Page</Button>
+                        <Button onClick={this.handler.bind(this)} value='/'>Back Page</Button>
                         <Button onClick={this.handleNextFeature.bind(this)}>Next</Button>
                     </div>}
                 {this.state.current > 1 && this.state.current < this.state.pages &&
@@ -78,10 +125,10 @@ class Feature extends Component {
                         <Button onClick={this.handleBackFeature}>Back</Button>
                         <Button onClick={this.handleNextFeature.bind(this)}>Next</Button>
                     </div>}
-                {this.state.current == this.state.pages && this.state.current > 1 &&
+                {this.state.current === this.state.pages && this.state.current > 1 &&
                     <div>
                         <Button onClick={this.handleBackFeature}>Back</Button>
-                        <Button onClick={this.props.nextHandler}>Next Page</Button>
+                        <Button onClick={this.handler.bind(this)} value='/'>Next Page</Button>
                     </div>}
             </div>
         );
