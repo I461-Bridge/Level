773d89c64b7cf7d8825a2cb665e4d9ab2210c095
sopheak <sopheakneak@live.com>
2017-06-04 16:37:00 -0700

    fixed some specs

--- level-app/src/MainSelection.js
+++ level-app/src/MainSelection.js
@@ -12,9 +12,9 @@ class MainSelection extends Component {
             selectedClass: '',
             selectedLevel: '',
             selectedCombo: {},
-            classButtonTitle: "Character Class",
+            classButtonTitle: "Select Character Class",
             levelsArray: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-            levelButtonTitle: "",
+            levelButtonTitle: "Select ",
             redirect: false
         }
     }
